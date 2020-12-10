fc7e61affbf214a7e6884861962bb09f83557a9e
Andrew Bruner <andrewbruner@gmail.com>
2020-12-10 03:03:18 +0000

    Refactor to Promise chaining

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// Environment Variables
+// Password Setup
 require('dotenv').config();
 
-// MongoDB/Mongoose
+// Database Setup (MongoDB/Mongoose)
 const mongoose = require('mongoose');
 const url = `mongodb+srv://admin:${process.env.PASSWORD}@blog.svxno.mongodb.net/blog?retryWrites=true&w=majority`;
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,40 +10,43 @@ db.on('error', console.error.bind(console, 'connection error:'));
 const postSchema = new mongoose.Schema({ title: String, body: String });
 const Post = mongoose.model('Post', postSchema);
 
-// Express
+// Server Setup (Express)
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const notFound = { message: '404: Page not found.'};
+const notFound = { message: '404: Page not found.' };
 
-// Routes
+// Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/api', async (req, res) => {
-    const posts = await Post.find();
-    posts.reverse();
-    res.json(posts);
+// Routes
+app.get('/api', (req, res) => {
+    Post.find()
+        .then(posts => posts.reverse())
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json(notFound));
 });
 
-app.get('/api/posts/:postId', async (req, res) => {
-    const post = await Post.findById(req.params.postId).catch(err => 'error');
-    post != 'error' ? res.json(post) : res.status(404).json(notFound);
+app.get('/api/posts/:postId', (req, res) => {
+    Post.findById(req.params.postId)
+        .then(post => post ? res.json(post) : res.status(404).json(notFound))
+        .catch(err => res.status(404).json(notFound));
 });
 
-app.post('/', async (req, res) => {
-    await Post.create({ title: req.body.newPostTitle, body: req.body.newPostBody}, (err, post) => {
-        err ? console.error(err) : console.log(`${post.title} saved successfully!`);
-    });
-    res.redirect('/');
-})
+app.post('/', (req, res) => {
+    new Post({ title: req.body.newPostTitle, body: req.body.newPostBody }).save()
+        .then(post => console.log(`${post.title} created successfully!`))
+        .then(post => res.redirect('/'))
+        .catch(err => console.error(err));
+});
 
 app.use((req, res) => {
     res.status(404).json(notFound);
 });
 
-// Server
+// Start Server
 app.listen(port, () => {
-    console.log(`Blog server listening on port ${port}`);
+    console.log(`Weblog server listening on port ${port}`);
 });
