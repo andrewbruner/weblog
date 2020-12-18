00779d6f2c9c1e194e578deac2b2df8be84f0926
Andrew Bruner <andrewbruner@gmail.com>
2020-12-18 15:34:31 +0000

    Make prettier

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Database Setup (MongoDB/Mongoose)
 const mongoose = require('mongoose');
 const url = `mongodb+srv://admin:${process.env.PASSWORD}@blog.svxno.mongodb.net/blog?retryWrites=true&w=majority`;
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 const postSchema = new mongoose.Schema({ title: String, body: String });
@@ -28,21 +28,23 @@ app.use(cookieParser());
 // API Routes
 app.get('/api', (req, res) => {
     Post.find()
-        .then(posts => posts.reverse())
-        .then(posts => res.json(posts))
-        .catch(err => res.status(404).json(notFound));
+        .then((posts) => posts.reverse())
+        .then((posts) => res.json(posts))
+        .catch((err) => res.status(404).json(notFound));
 });
 
 app.get('/api/posts/:postId', (req, res) => {
     Post.findById(req.params.postId)
-        .then(post => post ? res.json(post) : res.status(404).json(notFound))
-        .catch(err => res.status(404).json(notFound));
+        .then((post) =>
+            post ? res.json(post) : res.status(404).json(notFound)
+        )
+        .catch((err) => res.status(404).json(notFound));
 });
 
 // Authentication Route
 app.post('/authenticate', (req, res) => {
     User.findOne({ username: req.body.username })
-        .then(user => {
+        .then((user) => {
             if (user.password == req.body.password) {
                 res.cookie('authenticated', 'true');
                 if (req.cookies.redirectTo) {
@@ -52,26 +54,29 @@ app.post('/authenticate', (req, res) => {
                 }
             } else {
                 res.redirect('/authenticate');
-            };
+            }
         })
-        .catch(err => res.status(404).json(notFound));
+        .catch((err) => res.status(404).json(notFound));
 });
 
 // Static Routes (Public/Private)
 app.use(express.static('public'));
 app.use((req, res, next) => {
-    req.cookies.authenticated == 'true' ? next() : (() => {
-        res.cookie('redirectTo', req.path);
-        res.redirect('/authenticate');
-    })();
+    req.cookies.authenticated == 'true'
+        ? next()
+        : (() => {
+              res.cookie('redirectTo', req.path);
+              res.redirect('/authenticate');
+          })();
 }, express.static('private'));
 
 // Application Routes
 app.post('/new-post', (req, res) => {
-    new Post({ title: req.body.newPostTitle, body: req.body.newPostBody }).save()
-        .then(post => console.log(`${post.title} created successfully!`))
-        .then(post => res.redirect('/'))
-        .catch(err => console.error(err));
+    new Post({ title: req.body.newPostTitle, body: req.body.newPostBody })
+        .save()
+        .then((post) => console.log(`${post.title} created successfully!`))
+        .then((post) => res.redirect('/'))
+        .catch((err) => console.error(err));
 });
 
 // 404 Route
